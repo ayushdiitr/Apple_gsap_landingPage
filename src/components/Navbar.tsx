51265e1c88cb1bf565c976c55c706a9300784bfc
ayushdiitr <ayushd175@gmail.com>
2025-11-09 23:00:15 +0530

    feat: navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+
+    
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple logo" />
+
+                <ul>
+                    {
+                        navLinks.map((it) => (
+                            <li key={it.label} >
+                                <a href={it.href}>{it.label}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                <div className="flex-center gap-3">
+                    <button>
+                        <img src="/search.svg" alt="search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="search" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
